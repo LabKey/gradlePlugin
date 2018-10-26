4e3b9893c4108c3f1e27136f87e8627f37f131e3
labkey-susanh <susanh@labkey.com>
2018-10-26 09:39:55 -0700

    - Handle alpha branch names - Handle artifact versions with a patch version (X.Y.Z)

--- src/main/groovy/org/labkey/gradle/util/BuildUtils.groovy
+++ src/main/groovy/org/labkey/gradle/util/BuildUtils.groovy
@@ -45,7 +45,7 @@ class BuildUtils
     public static final String TEST_MODULES_DIR = "server/test/modules"
 
     // the set of modules required for minimal LabKey server functionality
-    // (aside from the bootstrap, api, internal, schemas and remoteapi projects
+    // (aside from the bootstrap, api, internal, and remoteapi projects
     // whose paths are parameterized in the settings.gradle file)
     private static final List<String> BASE_MODULES =
             [
@@ -266,10 +266,11 @@ class BuildUtils
         if (project.hasProperty("versioning"))
         {
             String rootBranch = project.rootProject.versioning.info.branchId
+            String lowerBranch = rootBranch.toLowerCase()
 
             if (!["trunk", "master", "develop", "none"].contains(rootBranch))
             {
-                if (rootBranch.toLowerCase().startsWith("sprint"))
+                if (lowerBranch.startsWith("sprint"))
                 {
                     version = version.replace("-SNAPSHOT", "")
                     version += "Sprint"
@@ -278,9 +279,17 @@ class BuildUtils
                         project.logger.error("Root branch name '${rootBranch}' not as expected.  Distribution name may not be as expected.");
                     else
                         version += nameParts[2]
-
                 }
-                else if (rootBranch.toLowerCase().startsWith("release") &&
+                else if (lowerBranch.matches(".*-alpha.*"))
+                {
+                    version = version.replace("-SNAPSHOT", "")
+                    String[] nameParts = rootBranch.split("-")
+                    if (nameParts.length < 2)
+                        project.logger.error("Root branch name '${rootBranch}' not as expected.  Distribution name may not be as expected.");
+                    else
+                        version += "-" + nameParts[nameParts.length-1]
+                }
+                else if (lowerBranch.startsWith("release") &&
                         project.labkeyVersion.contains("-SNAPSHOT"))
                 {
                     version = version.replace("-SNAPSHOT", "Beta");
@@ -324,6 +333,12 @@ class BuildUtils
         return version
     }
 
+    /**
+     * Returns a module version to be used as a LabKey Module property.  This must be a decimal
+     * number (e.g., 19.1), so we use only the first two parts of the artifact version number
+     * @param project The project whose artifact version is to be translated to a LabKey module version
+     * @return the LabKey module version string
+     */
     static String getLabKeyModuleVersion(Project project)
     {
         String version = project.version
@@ -331,6 +346,9 @@ class BuildUtils
         Matcher matcher = Pattern.compile("([^_-]+)[_-].*").matcher(version)
         if (matcher.matches())
             version = matcher.group(1)
+        String[] versionParts = version.split("\\.")
+        if (versionParts.size() > 2)
+            version = versionParts[0] + "." + versionParts[1]
         return version
     }
 
