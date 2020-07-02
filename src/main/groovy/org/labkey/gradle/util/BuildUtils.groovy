0219ad76ecad67f88995ae0ec20b752881e85b1e
labkey-susanh <susanh@labkey.com>
2020-07-02 14:04:46 -0700

    Remove obsolte directory name constants

--- src/main/groovy/org/labkey/gradle/util/BuildUtils.groovy
+++ src/main/groovy/org/labkey/gradle/util/BuildUtils.groovy
@@ -39,8 +39,6 @@ class BuildUtils
     public static final String PLATFORM_MODULES_DIR = "server/modules/platform"
     public static final String COMMON_ASSAYS_MODULES_DIR = "server/modules/commonAssays"
     public static final String CUSTOM_MODULES_DIR = "server/modules/customModules"
-    public static final String OPTIONAL_MODULES_DIR = "server/optionalModules" // TODO remove after 20.6
-    public static final String EXTERNAL_MODULES_DIR = "externalModules" // TODO remove after 20.6
 
 
     public static final List<String> EHR_MODULE_NAMES = [
