52e0845f6915d860414c7b1a05d98af77e609445
labkey-tchad <tchad@labkey.com>
2020-07-13 13:51:14 -0700

    Validate task on execution instead of construction

--- src/main/groovy/org/labkey/gradle/task/CopyAndInstallRPackage.groovy
+++ src/main/groovy/org/labkey/gradle/task/CopyAndInstallRPackage.groovy
@@ -3,27 +3,22 @@ package org.labkey.gradle.task
 import org.gradle.api.file.CopySpec
 import org.gradle.api.tasks.InputDirectory
 import org.gradle.api.tasks.TaskAction
-import org.gradle.api.tasks.TaskInstantiationException
+import org.gradle.api.tasks.TaskExecutionException
 
 class CopyAndInstallRPackage extends InstallRPackage
 {
     @InputDirectory
     File packageLocation
 
-    private String packageName
-
-    CopyAndInstallRPackage()
-    {
-        if (getPackageNames() == null || getPackageNames().isEmpty())
-        {
-            throw new TaskInstantiationException("Task did not specify a package to install.")
-        }
-        packageName = getPackageNames().get(0)
-    }
-
     @TaskAction
     void doInstall()
     {
+        if (getPackageNames() == null || getPackageNames().isEmpty())
+        {
+            throw new TaskExecutionException(this, new RuntimeException("Task did not specify a package to install."))
+        }
+        String packageName = getPackageNames().get(0)
+
         super.doInstall() // Install dependencies
         File rLibsUserDir = getInstallDir()
         project.copy {
