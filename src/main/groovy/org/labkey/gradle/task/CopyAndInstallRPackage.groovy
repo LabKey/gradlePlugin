3a7485aceebfb3f74df6e165a1fce493d788754a
labkey-tchad <tchad@labkey.com>
2020-07-13 13:26:46 -0700

    Try to use getter

--- src/main/groovy/org/labkey/gradle/task/CopyAndInstallRPackage.groovy
+++ src/main/groovy/org/labkey/gradle/task/CopyAndInstallRPackage.groovy
@@ -3,21 +3,22 @@ package org.labkey.gradle.task
 import org.gradle.api.file.CopySpec
 import org.gradle.api.tasks.InputDirectory
 import org.gradle.api.tasks.TaskAction
+import org.gradle.api.tasks.TaskInstantiationException
 
 class CopyAndInstallRPackage extends InstallRPackage
 {
     @InputDirectory
     File packageLocation
 
-    private final String packageName
+    private String packageName
 
     CopyAndInstallRPackage()
     {
-        if (packageNames == null || packageNames.isEmpty())
+        if (getPackageNames() == null || getPackageNames().isEmpty())
         {
-            logger.error("No package name specified.")
+            throw new TaskInstantiationException("Task did not specify a package to install.")
         }
-        packageName = packageNames.get(0)
+        packageName = getPackageNames().get(0)
     }
 
     @TaskAction
