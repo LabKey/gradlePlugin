2d9386685adb03ed5038e77d26ff46a654c010af
labkey-susanh <susanh@labkey.com>
2024-12-17 08:05:27 -0800

    call the method don't just reference it

--- src/main/groovy/org/labkey/gradle/plugin/Distribution.groovy
+++ src/main/groovy/org/labkey/gradle/plugin/Distribution.groovy
@@ -138,7 +138,7 @@ class Distribution implements Plugin<Project>
                     if (it instanceof DefaultProjectDependency)
                     {
                         DefaultProjectDependency dep = (DefaultProjectDependency) it
-                        TaskUtils.addOptionalTaskDependency(project.project(dep.getPath), distTask, "module")
+                        TaskUtils.addOptionalTaskDependency(project.project(dep.getPath()), distTask, "module")
                     }
                 }
             }
