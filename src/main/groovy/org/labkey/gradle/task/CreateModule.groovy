dfd84f501c288ace144c95d9939e805ca37ba58c
Adam Rauch <adam@labkey.com>
2025-01-07 13:29:46 -0800

    Update module template and dependency versions

--- src/main/groovy/org/labkey/gradle/task/CreateModule.groovy
+++ src/main/groovy/org/labkey/gradle/task/CreateModule.groovy
@@ -134,13 +134,13 @@ class CreateModule extends DefaultTask
         }
 
         String[] versionParts = BuildUtils.getLabKeyModuleVersion(project.rootProject).split("\\.")
-        String version = versionParts[0] + ".000"
         Map<String, String> substitutions = [
                 'MODULE_DIR_NAME' : moduleName.toLowerCase(),
                 'MODULE_LOWERCASE_NAME' : moduleName.toLowerCase(),
                 'MODULE_NAME' : StringUtils.capitalize(moduleName),
                 'CURRENT_YEAR': Calendar.getInstance().get(Calendar.YEAR).toString(),
-                'SCHEMA_VERSION_NUMBER': version
+                'STARTING_SCHEMA_VERSION_NUMBER': versionParts[0] + ".000",
+                'SCHEMA_VERSION_NUMBER': versionParts[0] + ".001"
         ]
 
         project.copy({ CopySpec copy ->
