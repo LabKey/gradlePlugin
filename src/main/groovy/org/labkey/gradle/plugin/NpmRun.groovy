942c7956f067dc5c3ed7aa7a065dd543d432d80a
labkey-susanh <susanh@labkey.com>
2025-08-11 09:33:35 -0700

    Remove `listNodeProjects` task

--- src/main/groovy/org/labkey/gradle/plugin/NpmRun.groovy
+++ src/main/groovy/org/labkey/gradle/plugin/NpmRun.groovy
@@ -170,24 +170,7 @@ class NpmRun implements Plugin<Project>
             }
         }
 
-        project.tasks.register("listNodeProjects") {
-            Task task ->
-                task.group = GroupNames.NPM_RUN
-                task.description = "List all projects that employ node in their build"
-                task.doLast({
-                    List<String> nodeProjects = []
-                    project.allprojects({Project p ->
-                        if (p.getPlugins().hasPlugin(NpmRun.class))
-                            nodeProjects.add(p.path)
-                    })
-                    if (nodeProjects.size() == 0)
-                        println("No projects found containing ${NPM_PROJECT_FILE}")
-                    else {
-                        println("The following projects use Node in their builds:\n\t${nodeProjects.join("\n\t")}\n")
-                    }
-                })
-                task.notCompatibleWithConfigurationCache("Needs to walk the project tree")
-        }
+
 
     }
 
