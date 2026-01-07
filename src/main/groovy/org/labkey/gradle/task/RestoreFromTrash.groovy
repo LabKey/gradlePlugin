6e878dc03af0fcdc884420327e775d33a64ad040
labkey-susanh <susanh@labkey.com>
2026-01-07 09:25:52 -0800

    Modify RestoreFromTrash task to read modules and versions from files

--- src/main/groovy/org/labkey/gradle/task/RestoreFromTrash.groovy
+++ src/main/groovy/org/labkey/gradle/task/RestoreFromTrash.groovy
@@ -8,66 +8,149 @@ import org.apache.hc.client5.http.impl.classic.HttpClients
 import org.apache.hc.core5.http.HttpStatus
 import org.gradle.api.DefaultTask
 import org.gradle.api.GradleException
-import org.gradle.api.Project
+import org.gradle.api.provider.Property
+import org.gradle.api.tasks.Input
+import org.gradle.api.tasks.Optional
 import org.gradle.api.tasks.TaskAction
-import org.labkey.gradle.plugin.Api
-import org.labkey.gradle.plugin.FileModule
-import org.labkey.gradle.plugin.JavaModule
-import org.labkey.gradle.plugin.Module
-import org.labkey.gradle.util.BuildUtils
+import org.labkey.gradle.util.TaskUtils
 
 import static org.labkey.gradle.task.PurgeArtifacts.Response
 
-// TODO for more flexibility and for configuration cache support, needs to be converted to read projects and versions
-// from a file, as for PurgeArtifacts
 class RestoreFromTrash extends DefaultTask
 {
     public static final String VERSION_PROPERTY = "restoreVersion"
+    public static final String VERSIONS_FILE_PROPERTY = 'restoreVersions'
+    public static final String RESTORE_LIST_FILE_PROPERTY = 'restoreList';
+
+    @Input @Optional
+    final abstract Property<String> restoreVersion = project.objects.property(String).convention(project.hasProperty(VERSION_PROPERTY) ? (String) project.property(VERSION_PROPERTY) : "")
+
+    @Input @Optional
+    final abstract Property<String> restoreVersions = project.objects.property(String).convention(project.hasProperty(VERSIONS_FILE_PROPERTY) ? (String) project.property(VERSIONS_FILE_PROPERTY) : "")
+    @Input @Optional
+    final abstract Property<String> restoreListFile = project.objects.property(String).convention(project.hasProperty(RESTORE_LIST_FILE_PROPERTY) ? (String) project.property(RESTORE_LIST_FILE_PROPERTY) : "")
+    @Input
+    final abstract Property<Boolean> isDryRun = project.objects.property(Boolean).convention(project.hasProperty(PurgeArtifacts.DRY_RUN_PROPERTY))
+
+    @Input
+    final abstract Property<String> artifactoryUrl = project.objects.property(String).convention((String) project.property('artifactory_contextUrl'))
+    @Input
+    final abstract Property<String> artifactoryUser = project.objects.property(String).convention((String) project.property('artifactory_user'))
+    @Input
+    final abstract Property<String> artifactoryPassword = project.objects.property(String).convention((String) project.property('artifactory_password'))
+
+    private static final String NUM_NOT_FOUND = "numNotFound"
+    private static final String NUM_RESTORED = "numRestored"
+    private static final String UNRESTORED_VERSIONS = "unrestoredVersions"
 
     @TaskAction
     void restoreVersions()
     {
-        String restoreVersion
-        if (!project.hasProperty(VERSION_PROPERTY))
-            throw new GradleException("No value provided for ${VERSION_PROPERTY}.")
-        restoreVersion = project.property(VERSION_PROPERTY)
-        String[] unrestoredVersions = []
-        int numRestored = 0
-        int numNotFound = 0
-        project.allprojects({ Project p ->
-            def plugins = p.getPlugins()
-            if (plugins.hasPlugin(Module.class) || plugins.hasPlugin(JavaModule.class) || plugins.hasPlugin(FileModule.class)) {
-                logger.quiet("Considering ${p.path}...")
-                Response response = makeRestoreRequest(p.name, restoreVersion, "module")
-                if (response == Response.NOT_FOUND)
-                    numNotFound++
-                else if (response == Response.ERROR) {
-                    unrestoredVersions += "${p.path} - module: ${restoreVersion}"
-                } else
-                    numRestored++
+        String restoreModulesFileName = restoreListFile.get();
+        if (StringUtils.isEmpty(restoreModulesFileName))
+            throw new GradleException("Use -P${RESTORE_LIST_FILE_PROPERTY}=<moduleNames.txt> to provide a list of modules to work with.")
+        List<String> moduleNames = TaskUtils.readInputFile(restoreModulesFileName, "modules", logger)
+        if (moduleNames.isEmpty())
+            throw new GradleException("No module names found in file ${restoreModulesFileName}")
+        String version = this.restoreVersion.get()
+        if (!StringUtils.isEmpty(version.trim()))
+            this.restoreVersion(version, moduleNames)
+        else
+        {
+            Map<String, Integer> overallStats = new HashMap<>()
+            overallStats.put(NUM_NOT_FOUND, 0)
+            overallStats.put(NUM_RESTORED, 0)
+            String restoreVersionsFileName = restoreVersions.get()
+            if (StringUtils.isEmpty(restoreVersionsFileName))
+                throw new GradleException("Either -P${VERSION_PROPERTY}=<versionToPurge> or -P${VERSIONS_FILE_PROPERTY}=<versionsFile.txt> must be provided")
+            List<String> versions = TaskUtils.readInputFile(restoreVersionsFileName, "versions", logger)
+            if (versions.isEmpty())
+                throw new GradleException("No versions found for file ${restoreVersionsFileName}.")
+            if (versions.size() > 1) {
+                for (String moduleName : moduleNames) {
+                    Map<String, Object> restoreStats = restoreModuleVersions(moduleName, versions)
+                    overallStats.put(NUM_NOT_FOUND, overallStats.get(NUM_NOT_FOUND) + (Integer) restoreStats.get(NUM_NOT_FOUND))
+                    overallStats.put(NUM_RESTORED, overallStats.get(NUM_RESTORED) + (Integer) restoreStats.get(NUM_RESTORED))
+                }
+                if (moduleNames.size() > 1)
+                    logger.quiet("\nSummary:\n\tRestored ${overallStats.get(NUM_RESTORED)} artifacts.\n\t${overallStats.get(NUM_NOT_FOUND)} artifacts not found.")
             }
-            if (plugins.hasPlugin(Api.class) || project.path == BuildUtils.getApiProjectPath(project.gradle)) {
-                Response response = makeRestoreRequest(p.name, restoreVersion, "api")
-                if (response == Response.NOT_FOUND)
-                    numNotFound++
-                else if (response == Response.ERROR) {
-                    unrestoredVersions += "${p.path} - api: ${restoreVersion}"
-                } else
-                    numRestored++
+            else {
+                for (String v : versions) {
+                    if (!StringUtils.isEmpty(v.trim()))
+                    this.restoreVersion(v, moduleNames)
+                }
             }
-        })
+        }
+    }
 
-        logger.quiet("Restored ${numRestored} artifacts; ${numNotFound} artifacts not found.")
-        if (unrestoredVersions.size() > 0 && !project.hasProperty("dryRun"))
-            throw new GradleException("The following ${unrestoredVersions.size()} versions were not restored.\n${StringUtils.join(unrestoredVersions, "\n")}\nCheck the log for more information.")
+    Map<String, Object> restoreModuleVersions(String moduleName, List<String> versions)
+    {
+        Map<String, Object> restoreStats = new HashMap();
+        restoreStats.put(NUM_RESTORED, 0)
+        restoreStats.put(NUM_NOT_FOUND, 0)
+        restoreStats.put(UNRESTORED_VERSIONS, new ArrayList<>())
 
+        logger.quiet("### Begin restore for module ${moduleName} for ${versions.size()} versions\n")
+
+        for (String version: versions) {
+            if (!StringUtils.isEmpty(version.trim()))
+                makeRequests(moduleName, moduleName, version, restoreStats)
+        }
+        logStats(restoreStats)
+        logger.quiet("\n### End restore for module ${moduleName}\n")
+        return restoreStats
+    }
+
+    Map<String, Object> restoreVersion(String version, List<String> moduleNames)
+    {
+        if (StringUtils.isEmpty(version.trim()))
+            return
+        Map<String, Object> restoreStats = new HashMap();
+        restoreStats.put(NUM_RESTORED, 0)
+        restoreStats.put(NUM_NOT_FOUND, 0)
+        restoreStats.put(UNRESTORED_VERSIONS, new ArrayList<>())
+        for (String moduleName: moduleNames) {
+            makeRequests(moduleName, moduleName, version, restoreStats)
+        }
+        logStats(restoreStats)
+        return restoreStats
+    }
+
+    void logStats(Map<String, Object> restoreStats)
+    {
+        logger.quiet("Restored ${restoreStats.get(NUM_RESTORED)} artifacts; ${restoreStats.get(NUM_NOT_FOUND)} artifacts not found.")
+        if (((List<String>) restoreStats.get(UNRESTORED_VERSIONS)).size() > 0 && !isDryRun.get())
+            throw new GradleException("The following ${((List<String>) restoreStats.get(UNRESTORED_VERSIONS)).size()} versions were not restored.\n${StringUtils.join(restoreStats.get(UNRESTORED_VERSIONS), "\n")}\nCheck the log for more information.")
+    }
+
+    void makeRequests(String moduleName, String loggingName, String restoreVersion, Map<String, Object> statsMap)
+    {
+        if (StringUtils.isEmpty(restoreVersion.trim()))
+            return
+        logger.quiet("Considering ${loggingName} ${restoreVersion}...")
+        Response response = makeRestoreRequest(moduleName, restoreVersion, "module")
+        if (response == Response.NOT_FOUND)
+            statsMap.put(NUM_NOT_FOUND, statsMap.get(NUM_NOT_FOUND)+1);
+        else if (response == Response.ERROR)
+            statsMap.get(UNRESTORED_VERSIONS).add("${loggingName} - module: ${restoreVersion}")
+        else
+            statsMap.put(NUM_RESTORED, statsMap.get(NUM_RESTORED)+1)
+
+        response = makeRestoreRequest(moduleName, restoreVersion, "api")
+        if (response == Response.NOT_FOUND)
+            statsMap.put(NUM_NOT_FOUND, statsMap.get(NUM_NOT_FOUND)+1)
+        else if (response == Response.ERROR)
+            statsMap.get(UNRESTORED_VERSIONS).add("${loggingName} - api: ${restoreVersion}")
+        else
+            statsMap.put(NUM_RESTORED, statsMap.get(NUM_RESTORED)+1)
     }
 
     /**
      * This uses the Artifactory REST Api to request a restoration of a particular "item" (https://jfrog.com/help/r/jfrog-rest-apis/restore-item-from-trash-can)
      *
-     * @param artifactName the artifact whose version is to be deleted
-     * @param version the version of the artifact to delete (e.g., 21.11-SNAPSHOT)
+     * @param artifactName the artifact whose version is to be restored
+     * @param version the version of the artifact to restore (e.g., 21.11-SNAPSHOT)
      * @param type either "api" or "module"
      * @return Response summarizing http status from request
      * @throws GradleException if the restoration request throws an exception
@@ -76,7 +159,7 @@ class RestoreFromTrash extends DefaultTask
     {
         if (project.hasProperty("dryRun")) {
             logger.quiet("\tRestoring version ${version} of ${artifactName} ${type} -- Skipped for dry run")
-            return
+            return null
         }
 
         CloseableHttpClient httpClient = HttpClients.createDefault()
