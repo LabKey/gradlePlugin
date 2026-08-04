387f936448f5c5cb7e4b61d85699213aff8eb96d
labkey-susanh <susanh@labkey.com>
2026-08-04 15:50:31 -0700

    Make UndeployModules compatible with configuration cache

--- src/main/groovy/org/labkey/gradle/plugin/ServerDeploy.groovy
+++ src/main/groovy/org/labkey/gradle/plugin/ServerDeploy.groovy
@@ -237,7 +237,6 @@ class ServerDeploy implements Plugin<Project>
             UndeployModules task ->
                 task.group = GroupNames.DEPLOY
                 task.description = "Removes all module files and directories from the deploy and staging directories"
-                task.notCompatibleWithConfigurationCache("Walks the project tree")
         }
 
         project.tasks.register(
